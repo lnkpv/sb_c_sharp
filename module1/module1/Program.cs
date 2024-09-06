e13eba78d52045840296c99174769e474b7a5153
lnkpv <a.yakupova2004@yandex.ru>
2024-09-06 14:42:30 +0300

    module 1 init

---
+++ module1/module1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            Console.WriteLine("Hello World!!!");
+
+            Console.ReadKey();
+
+            // Task 2
+
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
