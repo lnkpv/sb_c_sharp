79a1a6cebf102fb52cf75f1e4c3cafa5903405ed
lnkpv <a.yakupova2004@yandex.ru>
2024-09-11 19:01:48 +0300

    init module2

---
+++ module2/module2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1.
+            string fullName = "Иванов Иван Иванович";
+            int age = 20;
+            string email = "ivanov@test.com";
+            double programmingScore = 88.5;
+            double mathScore = 91.0;
+            double physicsScore = 73.5;
+
+            Console.WriteLine($"Информация о студенте: " +
+                $"\nФИО: {fullName} " +
+                $"\nВозраст: {age} " +
+                $"\nEmail: {email} " +
+                $"\nБаллы по программированию: {programmingScore} " +
+                $"\nБаллы по математике: {mathScore} " +
+                $"\nБаллы по физике: {physicsScore}");
+
+            Console.ReadKey();
+
+            // Задание 2.
+            double totalScore = programmingScore + mathScore + physicsScore;
+            double averageScore = totalScore / 3;
+
+            // Для среднего значения решила вывести 4 знака после запятой
+            Console.WriteLine($"\nРезультаты: " +
+                $"\nСумма баллов по всем предметам: {totalScore} " +
+                $"\nСредний балл: {averageScore:#.####}");
+
+            Console.ReadKey();
+        }
+    }
+}
