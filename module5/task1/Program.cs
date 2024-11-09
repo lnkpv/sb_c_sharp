56c9e5f111ef36b0ce57f2e035240e6faa020b23
lnkpv <a.yakupova2004@yandex.ru>
2024-11-10 00:32:55 +0300

    [add] module 5

---
+++ module5/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string inputText = Console.ReadLine();
+
+            string[] words = SplitText(inputText);
+
+            DisplayWords(words);
+            Console.ReadKey();
+        }
+
+        static string[] SplitText(string text)
+        {
+            char[] separator = new char[] { ' ' };
+            return text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void DisplayWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ module5/task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+
+            string reversedPhrase = ReverseWords(inputPhrase);
+            Console.WriteLine("Результат:");
+            Console.WriteLine(reversedPhrase);
+            Console.ReadKey();
+        }
+
+        static string ReverseWords(string text)
+        {
+            string[] words = SplitText(text);
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+
+        static string[] SplitText(string text)
+        {
+            char[] separator = new char[] { ' ' };
+            return text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
